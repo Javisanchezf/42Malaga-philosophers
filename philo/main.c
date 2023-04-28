ec3c53adef4c41ad3778feffb9f8fb184a44da02
Javier Sánchez Ferández <javiersanchezf@hotmail.com>
2023-04-28 21:57:03 +0200

    Little changes

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: javiersa <javiersa@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 17:58:04 by javiersa          #+#    #+#             */
-/*   Updated: 2023/04/28 21:45:56 by javiersa         ###   ########.fr       */
+/*   Updated: 2023/04/28 21:52:47 by javiersa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,10 +35,10 @@ t_data	parse_data(int narg, char **argv)
 	check_args(narg, argv);
 	data.ends = malloc (1 * sizeof(short int));
 	if (!data.ends)
-		ft_error("Error", 0);
+		ft_error("Error allocating memory.", 0);
 	data.stop = malloc (1 * sizeof(short int));
 	if (!data.ends)
-		ft_error("Error", 1, data.ends);
+		ft_error("Error allocating memory.", 1, data.ends);
 	*(data.ends) = 0;
 	*(data.stop) = 1;
 	data.n_philos = ft_atoui(argv[1]);
@@ -55,21 +55,23 @@ t_data	parse_data(int narg, char **argv)
 
 void *philo_do(void *arg)
 {
-    t_philos *philo = (t_philos *)arg;
-    t_data *data = philo->data;
-    
-    printf("Philosopher %d data:\n", philo->id);
-    printf("n_philos: %u\n", data->n_philos);
-    printf("time_dead: %u\n", data->time_dead);
-    printf("time_eat: %u\n", data->time_eat);
-    printf("time_sleep: %u\n", data->time_sleep);
-    printf("n_eats: %u\n", data->n_eats);
-    printf("ends: %p\n", (void *)data->ends);
-    printf("stop: %p\n", (void *)data->stop);
-    printf("ends: %hd\n", *(data->ends));
-    printf("stop: %hd\n", *(data->stop));
-    
-    pthread_exit(NULL);
+	t_philos	*philo;
+    t_data		*data;
+
+	philo = (t_philos *)arg;
+	data = philo->data;
+	printf("Philosopher %d data:\n", philo->id);
+	printf("n_philos: %u\n", data->n_philos);
+	printf("time_dead: %u\n", data->time_dead);
+	printf("time_eat: %u\n", data->time_eat);
+	printf("time_sleep: %u\n", data->time_sleep);
+	printf("n_eats: %u\n", data->n_eats);
+	printf("ends: %p\n", (void *)data->ends);
+	printf("stop: %p\n", (void *)data->stop);
+	printf("ends: %hd\n", *(data->ends));
+	printf("stop: %hd\n", *(data->stop));
+
+	pthread_exit(NULL);
 }
 
 
@@ -83,24 +85,20 @@ t_philos	*philo_born(t_data *data)
 	//Prevenir si filosofos == 1 esperar al timedead
 	philo = malloc(data->n_philos * sizeof(t_philos));
 	if (!philo)
-		ft_error("Error", 2, data->ends, data->stop);
-	while(++i < data->n_philos)
+		ft_error("Error allocating memory.", 2, data->ends, data->stop);
+	while (++i < data->n_philos)
 	{
 		philo[i].id = i;
 		philo[i].data = data;
-		// philo[i].data->stop = data->stop;
-		// philo[i].data->ends = data->ends;
-		//printf("Data philo Nº%u: data.n_philos: %u, data.time_dead: %u,data.time_eat:%u ,data.time_sleep: %u\n",i, philo[i].data.n_philos, philo[i].data.time_dead, philo[i].data.time_eat, philo[i].data.time_sleep);
-		printf("Data philo Nº%u: data.n_philos: %p, data.time_dead: %u,data.time_eat:%u ,data.time_sleep: %u\n",i, &philo[i].data->n_philos, philo[i].data->time_dead, philo[i].data->time_eat, philo[i].data->time_sleep);
 		if (pthread_create(&philo[i].thread, NULL, philo_do, &philo[i]))
 			ft_error("Error creating thread", 1, philo);
 		if (i != 0)
 			philo[i].fork_l = &philo[i - 1].fork_r;
 	}
 	philo[0].fork_l = &philo[i].fork_r;
-	for (i = 0; i < philo->data->n_philos; i++) {
-    pthread_join(philo[i].thread, NULL);
-}
+	i = -1;
+	while (++i < data->n_philos)
+		pthread_join(philo[i].thread, NULL);
 	return(philo);
 }
 
